33f8e5624bbaaedd15444704bb56225a1d115319
geomarian <geomarian520@gmail.com>
2022-09-09 23:48:48 +0530

    Added Stop watch program

---
+++ src/com/bridgelabz/StopWatch.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+/**
+ * 
+ */
+
+
+import java.util.Scanner;
+
+/**
+ * @author ASUS
+ *
+ */
+public class StopWatch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+        System.out.print("Press any Numkey to Start :");
+        input.nextFloat();
+        long startTime = System.currentTimeMillis();
+        System.out.println("Timer started...");
+
+        System.out.print("Press any Numkey to Stop :");
+        input.nextFloat();
+        long stoppedTime = System.currentTimeMillis();
+        long timeElapsed = (stoppedTime - startTime)/1000;
+        System.out.println("Timer stopped!");
+        
+        System.out.print("Elapsed time is " + timeElapsed);
+	}
+}
